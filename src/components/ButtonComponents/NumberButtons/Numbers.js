6211494609f8f3d3ef9798ea84cc261b869f56f9
Phong Yang <phongyang88@hotmail.com>
2019-12-10 21:08:01 -0600

    changed numbers import from numbers file to app file and passing the data as a property

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import {NumberButton} from "./NumberButton";
-import {numbers} from "../../../data";
+// import {numbers} from "../../../data";
 
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
@@ -11,7 +11,7 @@ import { numbers } from '../../../data'
 
 export const Numbers = props => {
   // STEP 2 - add the imported data to state
-  const [numberButton, setNumbers] = useState(numbers);
+  const [numberButton, setNumbers] = useState(props.numbers);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
